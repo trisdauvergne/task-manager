2ad8d4166b4b479f68b244812e667f6a6635638f
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-30 16:08:27 +0000

    200330 - 18:08 created gitignore and created environment

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+
+print(os.environ.get("MONGO_URI"))
+app.secret_key = os.environ.get("MONGO_URI")
 
 @app.route('/')
 def hello():
