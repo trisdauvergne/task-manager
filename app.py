65327ee42057f91ca981e616886cceb868c2ed84
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-31 09:41:11 +0000

    200331 - 11:41 fixed mongo uri issue and task displaying

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -9,15 +9,21 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-print(os.environ.get("MONGO_URI"))
-app.secret_key = os.environ.get("MONGO_URI")
+# print(os.environ.get("MONGO_URI"))
+# app.secret_key = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
+    app.run(host='0.0.0.0',
+    port=5000,
     debug=True)
